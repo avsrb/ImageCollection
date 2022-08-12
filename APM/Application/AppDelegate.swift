69c886902a9973adad9952fffe670cb063bf2240
Artem <Serebryakov>
2022-08-12 17:46:24 +0300

    init

---
+++ APM/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  APM
+//
+//  Created by Artem Serebriakov on 12.08.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        window?.overrideUserInterfaceStyle = .light
+        
+        return true
+    }
+}
