fbb06cc5abb476967176cb53cd7b87003f7db2a4
Artem <Serebryakov>
2022-08-12 21:58:48 +0300

    init

--- APM/Application/AppDelegate.swift
+++ APM/Application/AppDelegate.swift
@@ -14,8 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        let rootVC = ViewController()
+        let navVC = UINavigationController(rootViewController: rootVC)
+        navVC.navigationBar.tintColor = .white
+//        navVC.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        window?.rootViewController = navVC
         window?.makeKeyAndVisible()
+        
         window?.overrideUserInterfaceStyle = .light
         
         return true
--- APM/Controllers/ViewController.swift
+++ APM/Controllers/ViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var collectionView : UICollectionView = {
+        let collectionView = UICollectionView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    let idCell = "cell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Image"
+        view.backgroundColor = .white
     }
-
-
 }
+
+/*
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: idCell, for: IndexPath) as! UICollectionView
+    }
+}
+*/
 
