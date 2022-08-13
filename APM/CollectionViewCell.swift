877492e5832e253682d6803df670424c74da3f72
Artem <Serebryakov>
2022-08-13 15:59:12 +0300

    First version

---
+++ APM/CollectionViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  CollectionViewCell.swift
+//  APM
+//
+//  Created by Artem Serebriakov on 13.08.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    static let identifier = "CollectionViewCell"
+    
+    private var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        
+        let images = [
+            UIImage(named: "error"),
+            UIImage(named: "error"),
+            UIImage(named: "error"),
+            UIImage(named: "error")
+//            UIImage(systemName: "square.and.arrow.down"),
+//            UIImage(systemName: "home")
+        ].compactMap({ $0 })
+        imageView.image = images.randomElement()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+}
--- APM/Controllers/ViewController.swift
+++ APM/Controllers/ViewController.swift
@@ -9,30 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var collectionView : UICollectionView = {
-        let collectionView = UICollectionView()
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        return collectionView
-    }()
+//    private lazy var collectionView : UICollectionView = {
+//        let collectionView = UICollectionView()
+//        collectionView.translatesAutoresizingMaskIntoConstraints = false
+//        return collectionView
+//    }()
+    
+    private let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    )
     
     let idCell = "cell"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Image"
-        view.backgroundColor = .white
+//        view.backgroundColor = .systemBackground
+        
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        view.addSubview(collectionView)
+        
+//        collectionView.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
     }
 }
 
-/*
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: idCell, for: IndexPath) as! UICollectionView
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as! CollectionViewCell
+        return cell
     }
 }
-*/
-
